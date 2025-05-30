c6820006bd43c019e45fba674884af6b3ba5e812
Icarus <26633821+IcarusLivesHF@users.noreply.github.com>
2025-05-30 18:53:54 -0400

    Update 2bpp.bat

--- 2bpp.bat
+++ 2bpp.bat
@@ -4,16 +4,25 @@ rem benchmark 1
 set t1=%time%
 
 rem Script must be run by drag & drop  FOLDER  onto script.
-if "%~1" equ "" echo PATH required... & echo Drag ^& Drop   FOLDER   into %~nx0 to try again. & pause & exit
+if "%~1" equ "" (
+	echo PATH required...
+	echo Drag ^& Drop   FOLDER   into %~nx0 to try again.
+	pause & exit
+)
 
 call :init
 
 rem automatically name the sprite by the name of the PATH
-set "name=%~1" & set "name=!name:%cd%=!" & set "name=!name: =!" & set "name=!name:~1!"
+set "name=%~1"
+set "name=!name:%cd%=!"
+set "name=!name: =!"
+set "name=!name:~1!"
 
 rem temp change directory
 pushd %~1
 
+rem color palette / edit colors here, color[0] will be made transparent
+set /a "color[0]=0","color[1]=7","color[2]=15","color[3]=8"
 rem if palette file exists, use that color palette
 if exist palette.txt (
 	for /f "tokens=*" %%i in (palette.txt) do (
@@ -69,35 +78,37 @@ if not exist sprite.txt (
 			
 			rem make and stitch the 4 sprites together
 			%_2bpp% "!list1!"."!list2!"
-			set "%%~k=[y;xH!tile![8A[8C"
+			set "%%~k=%\e%7!tile!%\e%8%\e%[8C"
 			
 			%_2bpp% "!list3!"."!list4!"
-			set "%%~k=!%%~k!!tile![8D"
+			set "%%~k=!%%~k!%\e%7!tile!%\e%8%\e%[8B%\e%[8D"
 			
 			%_2bpp% "!list5!"."!list6!"
-			set "%%~k=!%%~k!!tile![8C[8A"
+			set "%%~k=!%%~k!%\e%7!tile!%\e%8%\e%[8C"
 			
 			%_2bpp% "!list7!"."!list8!"
-			set "%%~k=!%%~k!!tile![0m"
+			set "%%~k=!%%~k!!tile!%\e%[m"
 		
 			rem add transparency
 			for /l %%i in (8,-1,1) do (
 				for %%j in ("!spaceBuffer:~0,%%i!") do (
-					set "%%~k=!%%~k:[48;5;%color[0]%m%%~j=[%%~iC!"
+					set "%%~k=!%%~k:%\e%[48;5;%color[0]%m%%~j=%\e%[%%~iC!"
+
 				)
 			)
+			
+			
 		)
 	)
 	
 	rem capture raw output as useable variables
-	if not exist sprite.txt (
-		echo Saving data to variables...
-		for /l %%i in (0,1,!spriteFrames!) do (
-			echo set "%name%.frame%%i=!%name%.frame%%i!">>sprite.txt
-		)
+	echo Saving data to variables...
+	for /l %%i in (0,1,!spriteFrames!) do (
+		echo set "%name%.frame%%i=!%name%.frame%%i!">>sprite.txt
 	)
+	
 
-) else (
+ ) else (
 	rem if the sprite.txt already exist, just load this instead.
 	for /f "tokens=*" %%i in (sprite.txt) do (
 		set /a "spriteFrames+=1"
@@ -119,32 +130,42 @@ for /F "tokens=1-8 delims=:.," %%a in ("%t1: =0%:%t2: =0%") do set /a "td=(((1%%
 
 rem ### Main Loop ###################################################################
 
-:main
-	set /a "frames+=1", "id=frames %% (spriteFrames + 1)"
-	
-	echo [2J!%name%.frame%ID%:y;x=7;7![HLoad time: !td!cs ; ID: !id!
-	
-	for /l %%i in (1,%rotationSpeed%,1000000) do rem <-- throttle
-goto :main
+%@getTimeCS:?=t1%
+%while% ( %@getTimeCS:?=t2%, "deltatime=t2-t1"
+
+	if !deltatime! gtr 5 (
+		set /a "t1=t2", "frames=(frames + 1) %% (spriteFrames + 1)"
+		
+		for %%f in (!frames!) do echo %\e%[2J%\e%[7;7H!%name%.frame%%f!%\e%[HLoad time: !td!cs ; ID: %%f
+		
+	)
+)
 
 rem #################################################################################
 
 :init
 mode 28,28
 rem hide cursor
-<nul set /p "=[?25l"
+<nul set /p "=%\e%[?25l"
 rem necessary for transparency later
 set "spaceBuffer=                "
 
-rem color palette / edit colors here, color[0] will be made transparent
-set /a "color[0]=0","color[1]=7","color[2]=15","color[3]=8",   "rotationSpeed=10" & rem 1 = slowest
+
 
 rem newLine for macros
 (set \n=^^^
 %= This creates an escaped Line Feed - DO NOT ALTER =%
 )
 
-rem %_2bpp% "list1"."list2" where list1 & 2 contain 8 '2 digit' hex values
+for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a" %= \e =%
+
+set @getTimeCS=for /f "tokens=1-4 delims=:.," %%a in ("^!time: =0^!") do set /a "?=(((1%%a*60)+1%%b)*60+1%%c)*100+1%%d"
+
+set "while=for /l %%i in (1 1 16)do if defined do.while"
+set "while=set do.while=1&!while! !while! !while! !while! !while! "
+set "endWhile=set "do.while=""
+
+rem %_2bpp% "list1"."list2" where list1 & list2 contain 8x 2-digit hex values
 set _2bpp=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1,2 delims=." %%1 in ("^!args^!") do (%\n%
 	set "list[0]=%%~1" ^& set "list[1]=%%~2" ^& set "tile="%\n%
 	set "list[0]=^!list[0]:~2,-1^!"%\n%
@@ -155,14 +176,14 @@ set _2bpp=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1,2 delims=." %%1 in ("
 				set /a "_a=((0x%%~x >> %%b) & 1)", "_b=((0x%%~y >> %%b) & 1)"%\n%
 				set /a "c=(_b + _a * 2)"%\n%
 				if "^!lastc^!" neq "^!c^!" (%\n%
-					for %%c in (^^!c^^!) do set "tile=^!tile^![48;5;^!color[%%c]^!m "%\n%
+					for %%c in (^^!c^^!) do set "tile=^!tile^!%\e%[48;5;^!color[%%c]^!m "%\n%
 				) else (%\n%
 					set "tile=^!tile^! "%\n%
 				)%\n%
 				set "lastc=^!c^!"%\n%
 			)%\n%
 		)%\n%
-		set "tile=^!tile^![8D[B"%\n%
+		set "tile=^!tile^!%\e%[8D%\e%[B"%\n%
 	)%\n%
 )) else set args=
 goto :eof
